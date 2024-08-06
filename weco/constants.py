572ce74e1f3b0e1c89aea3cf3d030aa728806101
DhruvSrikanth <dhruvsrikanth5@gmail.com>
2024-08-06 11:02:33 +0100

    increase text limit

--- weco/constants.py
+++ weco/constants.py
@@ -1,4 +1,4 @@
-MAX_TEXT_LENGTH = 10000
+MAX_TEXT_LENGTH = 1e6  # 1 million characters
 MAX_IMAGE_UPLOADS = 5
 MAX_IMAGE_SIZE_MB = 10  # 10 MB
 SUPPORTED_IMAGE_EXTENSIONS = ["png", "jpeg", "jpg", "webp", "gif"]
